72b2d6fc2cccc403d5cc0099e2bb994911db95ad
violettemutuyimana247-cell <violettemutuyimana247@gmail.com>
2025-10-08 14:56:49 +0200

    Update App_in_Mind.py

--- App_in_Mind.py
+++ App_in_Mind.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 
-with open('Saving.pkl', 'rb') as file:
+with open('Save.pkl', 'rb') as file:
     Model = pickle.load(file)
 
 st.title("LAPTOP PRICE PREDICTION")
@@ -14,4 +14,5 @@ Storage = st.number_input("Storage size")
 if st.button("Predict"):
     input_ = np.array([[process, RAM, Storage]])
     Pred = Model.predict(input_)
+
     st.success(f"Price = {Pred[0]:.2f} RWF")  # Combine into a single string using f-string
