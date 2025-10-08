9df3376aa4a21aa7d87e9cc927f05928143ba77c
violettemutuyimana247-cell <violettemutuyimana247@gmail.com>
2025-10-08 12:51:34 +0200

    Update saved model.py

--- saved model.py
+++ saved model.py
@@ -8,11 +8,13 @@ This is a temporary script file.
 import pickle
 import pandas as pd
 import numpy as np
-loaded_model=pickle.load(open('C:/Users/user/Downloads/Lap top/Laptop_price.sav','rb'))
+with open('Laptop_price.sav','rb') as file:
+    loaded_model=pickle.load(file))
 laptop = pd.DataFrame([{
     "Processor_Speed": 3.830296,
     "RAM_Size":16,
     "Storage_Capacity":700
 }])
 prediction= loaded_model.predict(laptop)
+
 print(prediction)
